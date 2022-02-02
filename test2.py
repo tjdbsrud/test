10ab01c1ea89adbdaa47240dd9f8d6ca4c6cc3d7
YONGJIN SHIN <yongjin.shinn@gmail.com>
2022-02-02 15:22:36 +0900

    merged

--- test2.py
+++ test2.py
@@ -1,9 +1,6 @@
 print("Hello YK!")
 print('changed_YK!!!')
-<<<<<<< HEAD
 print("What is wrong with it?!")
 print('yeah~~~~~~~`')
-=======
 print("What are we goint to have as our dinner?!")
 print('herehere!!!!')
->>>>>>> 4a32de3ddd259890526f70d1d98d5f74072b5215
